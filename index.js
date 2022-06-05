2bd6262e810dae8b8bef4ef0b6f67753062d76b4
Tabitha Poppenwimer <poppenwimer.tkat@ufl.edu>
2022-06-05 22:56:48 +0300

    finished loop, cleaned up code

--- index.js
+++ index.js
@@ -30,29 +30,26 @@ remainingGuessDisplay.textContent = guessesLeft
 var word = words[Math.floor(Math.random() * words.length)];
 var guessed_word = [];
 for (var p = 0; p < word.length; p++){
-  guessed_word[p] = "_";
+  guessed_word.push("_");
 }
 
 var remainingLetters = word.length;
 
 word_to_guessEl.textContent = guessed_word.join("")
-/*
-var guessed_word = word.replace(/[a-z]/g, "_")
-word_to_guessEl.textContent = guessed_word*/
 
 var bad_letters = [];
 
 document.onkeyup = function(g){
-  var key = g.key.toLowerCase();
+  var guessed_key = g.key.toLowerCase()
   
-  var guessed_letter = word.indexOf(key)
+  var guessed_letter = word.indexOf(guessed_key)
   incorrect_lettersEl.textContent = guessed_letter
 
   if (guessed_letter > -1) {
     var updated_word = [];
     for (var i = 0; i < remainingLetters; i++) {
-      if(word[i] === key){
-      updated_word.push(key)
+      if(word[i] === guessed_key){
+      updated_word.push(guessed_key)
       } else{
         updated_word.push(guessed_word[i])
       }
@@ -60,40 +57,53 @@ document.onkeyup = function(g){
   
   guessed_word = updated_word
   word_to_guessEl.textContent = guessed_word.join("")
+  
   } else {
-    bad_letters.push(key)
-    guessesLeft--;
-    remainingGuessDisplay.textContent = guessesLeft
+    //A checker to see if the guess is in the 
+    var bad_check = bad_letters.indexOf(guessed_key);
+    
+
+    if(bad_check > -1){
+      //Letter has been already guessed NO CHANGE
+
+    }else if(!(/[a-zA-Z]/).test(guessed_key)){
+      //Letter is not actually a letter NO CHANGE
+
+    }else {
+      //Actual bad guess
+      bad_letters.push(guessed_key)
+      guessesLeft--;
+      remainingGuessDisplay.textContent = guessesLeft
+    }
   }
   incorrect_lettersEl.textContent = bad_letters
 
-// if(guessesLeft === 0){
-//   if(guessed_word.indexOf("_") > -1){
-//     lossesEl.textContent = losses++
-//   }else{
-//     winsEl.textContent = wins++
-//   }
-// }
 
-if(guessesLeft == 0 || guessed_word.indexOf("_") == -1){
-  if(guessed_word.indexOf("_") == -1){
+//Check if the game ends (either no guess or no "_")
+if(guessesLeft === 0 || guessed_word.indexOf("_") === -1){
+  
+  //Check if the game is a win (no "_")
+  if(guessed_word.indexOf("_") === -1){
     wins++
     winsEl.textContent = wins
   }
-  if(guessesLeft == 0){
+
+  //Check if the game is a loss (no guesses left)
+  if(guessesLeft === 0){
   losses++
-  lossesEl.textContent = losses
+  lossesEl.textContent = losses;
 }
   //show previous word
-  previous_wordEl.textContent = word
+  previous_wordEl.textContent = word;
 
   //random set next word
   word = words[Math.floor(Math.random() * words.length)];
+  remainingLetters = word.length;
 
   //change new word to underscore
   guessed_word = [];
-  for (var p = 0; p < word.length; p++){
-    guessed_word[p] = "_";
+  for (var p = 0; p < remainingLetters; p++){
+    guessed_word.push("_");
   }
   word_to_guessEl.textContent = guessed_word.join("")
 
@@ -108,9 +118,5 @@ if(guessesLeft == 0 || guessed_word.indexOf("_") == -1){
 
 }
 
-// if(guessed_word.indexOf("_") == -1){
-//   wins++
-//   winsEl.textContent = wins
-// }
 }
 

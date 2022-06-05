7d9574905dcd6c1f13e8b3cbccf817441c80916c
Tabitha Poppenwimer <poppenwimer.tkat@ufl.edu>
2022-06-05 21:19:32 +0300

    reset game, remove upload, reorder com.

--- index.js
+++ index.js
@@ -24,61 +24,93 @@ var wins = 0
 var losses = 0
 
 
-window.onload = function(){
+var guessesLeft = 10
+remainingGuessDisplay.textContent = guessesLeft
 
-  var guessesLeft = 10
-  remainingGuessDisplay.textContent = guessesLeft
+var word = words[Math.floor(Math.random() * words.length)];
+var guessed_word = [];
+for (var p = 0; p < word.length; p++){
+  guessed_word[p] = "_";
+}
 
-  var word = words[Math.floor(Math.random() * words.length)];
-  var guessed_word = [];
+var remainingLetters = word.length;
+
+word_to_guessEl.textContent = guessed_word.join("")
+/*
+var guessed_word = word.replace(/[a-z]/g, "_")
+word_to_guessEl.textContent = guessed_word*/
+
+var bad_letters = [];
+
+document.onkeyup = function(g){
+  var key = g.key.toLowerCase();
+  
+  var guessed_letter = word.indexOf(key)
+  incorrect_lettersEl.textContent = guessed_letter
+
+  if (guessed_letter > -1) {
+    var updated_word = [];
+    for (var i = 0; i < remainingLetters; i++) {
+      if(word[i] === key){
+      updated_word.push(key)
+      } else{
+        updated_word.push(guessed_word[i])
+      }
+  }
+  
+  guessed_word = updated_word
+  word_to_guessEl.textContent = guessed_word.join("")
+  } else {
+    bad_letters.push(key)
+    guessesLeft--;
+    remainingGuessDisplay.textContent = guessesLeft
+  }
+  incorrect_lettersEl.textContent = bad_letters
+
+// if(guessesLeft === 0){
+//   if(guessed_word.indexOf("_") > -1){
+//     lossesEl.textContent = losses++
+//   }else{
+//     winsEl.textContent = wins++
+//   }
+// }
+
+if(guessesLeft == 0 || guessed_word.indexOf("_") == -1){
+  if(guessed_word.indexOf("_") == -1){
+    wins++
+    winsEl.textContent = wins
+  }
+  if(guessesLeft == 0){
+  losses++
+  lossesEl.textContent = losses
+}
+  //show previous word
+  previous_wordEl.textContent = word
+
+  //random set next word
+  word = words[Math.floor(Math.random() * words.length)];
+
+  //change new word to underscore
+  guessed_word = [];
   for (var p = 0; p < word.length; p++){
     guessed_word[p] = "_";
   }
-
-  var remainingLetters = word.length;
-
   word_to_guessEl.textContent = guessed_word.join("")
-  /*
-  var guessed_word = word.replace(/[a-z]/g, "_")
-  word_to_guessEl.textContent = guessed_word*/
 
-  var bad_letters = [];
+  //reset incorrect letters
+  bad_letters = []
+  incorrect_lettersEl.textContent = bad_letters
 
- document.onkeyup = function(g){
-   var key = g.key.toLowerCase();
-   
-   guessesLeft--;
-   remainingGuessDisplay.textContent = guessesLeft
-   var guessed_letter = word.indexOf(key)
-   incorrect_lettersEl.textContent = guessed_letter
-
-   if (guessed_letter > -1) {
-     var updated_word = [];
-     for (var i = 0; i < remainingLetters; i++) {
-       if(word[i] === key){
-        updated_word.push(key)
-       } else{
-         updated_word.push(guessed_word[i])
-       }
-    }
-    guessed_word = updated_word
-    word_to_guessEl.textContent = guessed_word.join("")
-   } else {
-     bad_letters.push(key)
-   }
-   incorrect_lettersEl.textContent = bad_letters
-
-  if(guessesLeft === 0){
-    if(guessed_word.indexOf("_") > -1){
-      lossesEl.textContent = losses++
-    }else{
-      winsEl.textContent = wins++
-    }
-  }
- }
- 
-
-
+  //reset guesses to 10
+  guessesLeft = 10
+  remainingGuessDisplay.textContent = guessesLeft
 
 
 }
+
+// if(guessed_word.indexOf("_") == -1){
+//   wins++
+//   winsEl.textContent = wins
+// }
+}
+
